13006901ed2a8ae0e54b7c083987a2e56f87c113
nagokei <nagokei@gmail.com>
2016-02-04 11:22:36 +0000

    add user check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :check_user, only: [:show, :edit, :update]
+  
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -18,11 +20,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user
     else
@@ -31,6 +31,14 @@ class UsersController < ApplicationController
   end
   
   private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def check_user
+    redirect_to root_path if @user != current_user
+  end
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
