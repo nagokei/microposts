9b7783ecf70eadd9914436615aebc4b50f511f36
nagokei <nagokei@gmail.com>
2016-02-04 12:15:57 +0000

    add area

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,6 @@ class UsersController < ApplicationController
   end
   
   def user_params
-    params.require(:user).permit(:name, :email, :profile, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :profile, :area, :password, :password_confirmation)
   end
 end
