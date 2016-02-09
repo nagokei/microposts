7c61e7c773a44cab9b94185fc57d4b9c56fc79b1
nagokei <nagokei@gmail.com>
2016-02-09 23:37:33 +0000

    add kaminari

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :set_user, only: [:show, :edit, :update, :followings, :followers]
   def show
-    @microposts = @user.microposts.order(created_at: :desc)
+    @microposts = @user.microposts.order(created_at: :desc).page(params[:page])
   end
   
   def new
