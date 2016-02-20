8484ae94b91145e999f1cb2390a5394d0c6aa250
nagokei <nagokei@gmail.com>
2016-02-20 07:44:30 +0000

    change page count

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   
   def show
-    @microposts = @user.microposts.order(created_at: :desc).page(params[:page]).per(1)
+    @microposts = @user.microposts.order(created_at: :desc).page(params[:page]).per(3)
     
   end
   
